6da958683add785b9055dee464d1dc2683c6c19c
Aminnur <aminnur.ikhsan@gmail.com>
2026-01-30 14:39:39 +0700

    refactor: move inbound to DB

--- app/Livewire/Inbound.php
+++ app/Livewire/Inbound.php
@@ -2,45 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\Warehouse\IndboundModel;
 use Livewire\Component;
 
 class Inbound extends Component
 {
-    public $show = 1;
+    public $show = 10;
     public $search = '';
 
+    // Default Action
     public function render()
     {
-        $this->show = session()->get('show', 1);
+        $queries = IndboundModel::select(
+            'created_at as date',
+            '*'
+        );
 
-        $dataTable = collect([
-            ['id' => 1, 'date' => '2023-01-01', 'inbound_code' => 'INB001', 'origin_code' => 'ORI001', 'status' => 'requested'],
-            ['id' => 2, 'date' => '2023-01-02', 'inbound_code' => 'INB002', 'origin_code' => 'ORI002', 'status' => 'received'],
-            ['id' => 3, 'date' => '2023-01-03', 'inbound_code' => 'INB003', 'origin_code' => 'ORI003', 'status' => 'rejected'],
-            ['id' => 4, 'date' => '2023-01-04', 'inbound_code' => 'INB004', 'origin_code' => 'ORI004', 'status' => 'processed'],
-            ['id' => 5, 'date' => '2023-01-05', 'inbound_code' => 'INB005', 'origin_code' => 'ORI005', 'status' => 'done'],
-        ]);
+        if ($this->search) {
+            $search = $this->search;
+            $queries = $queries->where(function ($query) use ($search) {
+                $query->where('inbound_code', 'ilike', '%' . $search . '%')
+                    ->orWhere('origin_code', 'ilike', '%' . $search . '%');
+            });
+        }
 
-        $dataTable = $dataTable->filter(function ($item) {
-            $temp1 = str_contains(strtolower($item['inbound_code']), strtolower($this->search));
-            if ($temp1) {
-                return $temp1;
-            }
-            $temp2 = str_contains(strtolower($item['origin_code']), strtolower($this->search));
-            if ($temp2) {
-                return $temp2;
-            }
-            return false;
-        });
-
-        $dataTable = $dataTable->take($this->show);
+        $dataTable = $queries->orderBy('id', 'desc')->paginate($this->show);
 
         return view('livewire.inbound', compact('dataTable'));
-    }
-
-    function changeShow($value)
-    {
-        $this->show = $value;
-        session()->put('show', $value);
     }
 }
